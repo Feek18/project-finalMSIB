246426ee992b6849b18dfdb467003173b15d4332
Feek18 <bintangfikri31@gmail.com>
2024-06-05 03:27:25 +0800

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,7 +50,7 @@
                 <div class="row d-flex justify-content-center align-items-center" style="margin-top: 55px">
                     <div class="col">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="{{ asset('img/icon1.png') }}" alt="" class="img-fluid mb-3"
+                            <img src="{{ asset('img/icon1.png') }}" alt="" class="img-fluid mb-3 rounded-circle"
                                 style="max-width: 100px;">
                             <h3 class="mt-3">Pemesanan Mudah</h3>
                             <p>Sistem pemesanan intuitif untuk memesan lapangan dengan cepat dan tanpa harus repot serta ribet</p>
@@ -58,7 +58,7 @@
                     </div>
                     <div class="col">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="{{ asset('img/icon2.png') }}" alt="" class="img-fluid mb-3"
+                            <img src="{{ asset('img/icon2.png') }}" alt="" class="img-fluid mb-3 rounded-circle"
                                 style="max-width: 100px;">
                             <h3 class="mt-3">Lingkungan Profesional</h3>
                             <p>Lingkungan bermain yang ramah, staf yang siap membantu untuk pengalaman bermain yang
@@ -67,7 +67,7 @@
                     </div>
                     <div class="col">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="{{ asset('img/icon3.png') }}" alt="" class="img-fluid mb-3"
+                            <img src="{{ asset('img/icon3.png') }}" alt="" class="img-fluid mb-3 rounded-circle"
                                 style="max-width: 100px;">
                             <h3 class="mt-3">Fasilitas Premium</h3>
                             <p>Lapangan olahraga dengan fasilitas terbaik untuk pengalaman bermain yang optimal dan menyenangkan</p>
@@ -79,24 +79,7 @@
     </section>
 
     {{-- footer --}}
-    <footer style="background-color: #002379; height: auto; margin-top: 72px;">
-        <div class="container">
-            <div class="text-center p-3" style="">
-                <div class="d-flex justify-content-center align-items-center p-4 gap-3">
-                    <div class="col-md-4 d-flex text-start gap-3">
-                        <img src="{{ asset('img/logoaja.png') }}" width="20%" height="25%" alt="">
-                        <h2 class="text-white">SportSpotter</h2>
-                    </div>
-                    <div class="col-md-4 text-start">
-                        <p class="text-white">Jl. Merdeka No. 123, Jakarta 40374
-                            info@lapangansewa.com
-                            (021) 1234 5678</p>
-                    </div>
-                </div>
-                <p class="text-white mt-2 fs-6">© 2024 Lapangan Sewa. Semua Hak Dilindungi.</p>
-            </div>
-        </div>
-    </footer>
+    @include('layouts.footer')
 
 
 
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,19 @@
+{{-- footer --}}
+<footer style="background-color: #002379; height: auto; margin-top: 72px;">
+    <div class="container">
+        <div class="text-center p-3" style="">
+            <div class="d-flex justify-content-center align-items-center p-4 gap-3">
+                <div class="col-md-4 d-flex text-start gap-3">
+                    <img src="{{ asset('img/logoaja.png') }}" width="20%" height="25%" alt="">
+                    <h2 class="text-white">SportSpotter</h2>
+                </div>
+                <div class="col-md-4 text-start">
+                    <p class="text-white">Jl. Merdeka No. 123, Jakarta 40374
+                        info@lapangansewa.com
+                        (021) 1234 5678</p>
+                </div>
+            </div>
+            <p class="text-white mt-2 fs-6">© 2024 Lapangan Sewa. Semua Hak Dilindungi.</p>
+        </div>
+    </div>
+</footer>
