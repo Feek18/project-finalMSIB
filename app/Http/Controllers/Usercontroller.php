5768d363e6713c8e0434ed5cf0ac5bd55cce205e
zoe <markocop40@gmail.com>
2024-06-12 13:05:53 +0700

    penambahan namespace

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+use Illuminate\Http\Request;
 use App\Models\Lapangan;
-
 
 class UserController extends Controller
 {
@@ -19,16 +14,17 @@ class UserController extends Controller
         $lapangan = Lapangan::all();
         return view('home', compact('lapangan'));
     }
-     public function profil()
+
+    public function profil()
     {
         $user = Auth::user();
         return view('components.user.profil', compact('user'));
     }
 
-     public function updateProfil(Request $request)
+    public function updateProfil(Request $request)
     {
         $user = Auth::user();
-        
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
@@ -50,8 +46,9 @@ class UserController extends Controller
 
         return redirect()->route('profil')->with('success', 'Profile updated successfully');
     }
-    public function transaksi(Request $request){
+
+    public function transaksi(Request $request)
+    {
         return view('components.user.transaksi');
     }
-
 }
